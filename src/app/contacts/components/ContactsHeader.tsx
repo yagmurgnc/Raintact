564ba9c8abd8f467e3603b639b9a681d9f2246ba
Yağmur Genç <ygmrrkck@gmail.com>
2025-12-11 14:41:09 +0300

    Add icon to ContactsHeader

--- src/app/contacts/components/ContactsHeader.tsx
+++ src/app/contacts/components/ContactsHeader.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import { ArchiveBox } from 'iconsax-reactjs';
 
 export default function ContactsHeader() {
     return (
         <div>
             <Image src="/logo/logo.svg" alt="raintact" width={100} height={100}>
             </Image>
+            <ArchiveBox size="32" color="#697689" />
         </div>
+
     )
 }
